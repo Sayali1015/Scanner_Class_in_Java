993a72afb1494dabdc6959e58451fa86d1859a0d
Sayali <165777188+Sayali1015@users.noreply.github.com>
2025-12-31 13:00:56 +0530

    Add files via upload

---
+++ AreaOfCircle.java
@@ -0,0 +1,17 @@
+package Scanner_class_problems;
+import java.util.Scanner;
+public class AreaOfCircle {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Enter Number: ");
+		int num = sc.nextInt();
+		
+		double res = 3.14 * num;
+		System.out.println("Area of Circle is: "+ res);
+		
+
+	}
+
+}

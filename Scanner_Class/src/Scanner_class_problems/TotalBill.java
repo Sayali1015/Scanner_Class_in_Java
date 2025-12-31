1aea873bad318226c0e5b5df3f8e54ad0a4a126d
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-31 15:53:00 +0530

    Merge branch 'main' of https://github.com/Sayali1015/Scanner_Class_in_Java.git into main
    Total Bill code using Scanner Class

---
+++ Scanner_Class/src/Scanner_class_problems/TotalBill.java
@@ -0,0 +1,28 @@
+package Scanner_class_problems;
+import java.util.Scanner;
+public class TotalBill {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.println("Enter Product Name: ");
+		String name = scan.nextLine();
+		
+		System.out.println("Enter the Prize: ");
+		int prize = scan.nextInt();
+		
+		System.out.println("Enter the Quantity: ");
+		int totalQuan = scan.nextInt();
+		
+		
+		
+		int totalbill = prize*totalQuan;
+		
+		System.out.println("===========================================================");
+		System.out.println("Total Bill is: "+totalbill);
+		
+		
+
+	}
+
+}

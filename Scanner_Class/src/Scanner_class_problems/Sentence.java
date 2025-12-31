72d7ce4eeac4e103bf4a521c2998b4774804b3e7
Sayali Rajwade <sayalirajwade077@gmail.com>
2025-12-31 12:57:49 +0530

    Initial Commit

---
+++ Scanner_Class/src/Scanner_class_problems/Sentence.java
@@ -0,0 +1,15 @@
+package Scanner_class_problems;
+import java.util.Scanner;
+public class Sentence {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.print("Enter Sentence: ");
+		String sentence = sc.nextLine();
+		
+		System.out.println(sentence);
+
+	}
+
+}
